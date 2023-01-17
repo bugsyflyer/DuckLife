c23a4d84803f130eb97cf57065b0a219e1b2b2ab
bugsyflyer <brennaodonnell@oldrochester.org>
2023-01-17 08:32:14 -0500

    pc

--- Assets/Scripts/PC.cs
+++ Assets/Scripts/PC.cs
@@ -7,7 +7,7 @@ public class PC : MonoBehaviour
     // Start is called before the first frame update
     private Rigidbody2D playerRb;
     private GameObject player;
-    public float jumpForce = 5;
+    public float jumpForce = 10;
     public float gravityModifier = 1f;
     
     void Start()
@@ -19,25 +19,29 @@ public class PC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow)){
-            if (playerRb.velocity.y == 0){
-                playerRb.constraints &= ~RigidbodyConstraints2D.FreezePositionY;
-                playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            }
-        };
-        if (Input.GetKeyDown(KeyCode.DownArrow)){
-            if (playerRb.velocity.y == 0){
-                playerRb.constraints &= ~RigidbodyConstraints2D.FreezePositionY;
-                playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            }
-        };
-        
-        if (playerRb.transform.position.y < 0.1){
-            playerRb.AddForce(-Physics.gravity, ForceMode2D.Force);
+        if ((playerRb.transform.position.y !< 0.2 && playerRb.transform.position.y !> 0.2)){
+            playerRb.constraints &= ~RigidbodyConstraints2D.FreezePositionY;
         }
         else {
-            playerRb.constraints = RigidbodyConstraints2D.FreezePositionY;  
-        } ;
+            if (Input.GetKeyDown(KeyCode.UpArrow)){
+                if (playerRb.velocity.y == 0){
+                    playerRb.constraints &= ~RigidbodyConstraints2D.FreezePositionY;
+                    playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                }; 
+            } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+                if (playerRb.velocity.y == 0){
+                    playerRb.constraints &= ~RigidbodyConstraints2D.FreezePositionY;
+                    playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                }
+            }
+            else {
+                playerRb.constraints = RigidbodyConstraints2D.FreezePositionY;   
+            }
+
+        };
+        if (playerRb.transform.position.y < -0.2){
+            playerRb.AddForce(-Physics.gravity, ForceMode2D.Force);
+        }
 
     }
 
